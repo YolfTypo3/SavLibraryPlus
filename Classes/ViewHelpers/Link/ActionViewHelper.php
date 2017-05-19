76962f10627f0cd84c1e47b6990ea7706b0216b6
yolf <yolf.typo3@orange.fr>
2017-05-19 14:09:18 +0200

    ViewHelper modified.

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -33,69 +33,30 @@ use SAV\SavLibraryPlus\Managers\ExtensionConfigurationManager;
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class ActionViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
+class ActionViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\ActionViewHelper
 {
 
     /**
-     *
-     * @var string
-     */
-    protected $tagName = 'a';
-
-    /**
-     * Arguments initialization
-     *
-     * @return void
-     * @author Bastian Waidelich <bastian@typo3.org>
-     */
-    public function initializeArguments()
-    {
-        $this->registerUniversalTagAttributes();
-        $this->registerTagAttribute('name', 'string', 'Specifies the name of an anchor');
-        $this->registerTagAttribute('rel', 'string', 'Specifies the relationship between the current document and the linked document');
-        $this->registerTagAttribute('rev', 'string', 'Specifies the relationship between the linked document and the current document');
-        $this->registerTagAttribute('target', 'string', 'Specifies where to open the linked document');
-    }
-
-    /**
-     *
-     * @param string $action
-     *            Target action
-     * @param array $arguments
-     *            Arguments
-     * @param string $controller
-     *            Target controller. If NULL current controllerName is used
-     * @param string $extensionName
-     *            Target Extension Name (without "tx_" prefix and no underscores). If NULL the current extension name is used
-     * @param string $pluginName
-     *            Target plugin. If empty, the current plugin name is used
-     * @param integer $pageUid
-     *            target page. See TypoLink destination
-     * @param integer $pageType
-     *            type of the target page. See typolink.parameter
-     * @param boolean $noCache
-     *            set this to disable caching for the target page. You should not need this.
-     * @param boolean $noCacheHash
-     *            set this to supress the cHash query parameter created by TypoLink. You should not need this.
-     * @param string $section
-     *            the anchor to be added to the URI
-     * @param string $format
-     *            The requested format, e.g. ".html"
-     * @param boolean $linkAccessRestrictedPages
-     *            If set, links pointing to access restricted pages will still link to the page even though the page cannot be accessed.
-     * @param array $additionalParams
-     *            additional query parameters that won't be prefixed like $arguments (overrule $arguments)
-     * @param boolean $absolute
-     *            If set, the URI of the rendered link is absolute
-     * @param boolean $addQueryString
-     *            If set, the current query parameters will be kept in the URI
-     * @param array $argumentsToBeExcludedFromQueryString
-     *            arguments to be removed from the URI. Only active if $addQueryString = TRUE
+     * @param string $action Target action
+     * @param array $arguments Arguments
+     * @param string $controller Target controller. If NULL current controllerName is used
+     * @param string $extensionName Target Extension Name (without "tx_" prefix and no underscores). If NULL the current extension name is used
+     * @param string $pluginName Target plugin. If empty, the current plugin name is used
+     * @param int $pageUid target page. See TypoLink destination
+     * @param int $pageType type of the target page. See typolink.parameter
+     * @param bool $noCache set this to disable caching for the target page. You should not need this.
+     * @param bool $noCacheHash set this to suppress the cHash query parameter created by TypoLink. You should not need this.
+     * @param string $section the anchor to be added to the URI
+     * @param string $format The requested format, e.g. ".html
+     * @param bool $linkAccessRestrictedPages If set, links pointing to access restricted pages will still link to the page even though the page cannot be accessed.
+     * @param array $additionalParams additional query parameters that won't be prefixed like $arguments (overrule $arguments)
+     * @param bool $absolute If set, the URI of the rendered link is absolute
+     * @param bool $addQueryString If set, the current query parameters will be kept in the URI
+     * @param array $argumentsToBeExcludedFromQueryString arguments to be removed from the URI. Only active if $addQueryString = TRUE
+     * @param string $addQueryStringMethod Set which parameters will be kept. Only active if $addQueryString = TRUE
      * @return string Rendered link
-     * @author Sebastian Kurfürst <sebastian@typo3.org>
-     * @author Bastian Waidelich <bastian@typo3.org>
      */
-    public function render($action = NULL, array $arguments = array(), $controller = NULL, $extensionName = NULL, $pluginName = NULL, $pageUid = NULL, $pageType = 0, $noCache = FALSE, $noCacheHash = FALSE, $section = '', $format = '', $linkAccessRestrictedPages = FALSE, array $additionalParams = array(), $absolute = FALSE, $addQueryString = FALSE, array $argumentsToBeExcludedFromQueryString = array())
+    public function render($action = null, array $arguments = array(), $controller = null, $extensionName = null, $pluginName = null, $pageUid = null, $pageType = 0, $noCache = false, $noCacheHash = false, $section = '', $format = '', $linkAccessRestrictedPages = false, array $additionalParams = array(), $absolute = false, $addQueryString = false, array $argumentsToBeExcludedFromQueryString = array(), $addQueryStringMethod = null)
     {
 
         // Sets the new action
