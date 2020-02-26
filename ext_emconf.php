55995f1e57bde82b35e26b3f1dff8c8e0d974194
yolf <yolf.typo3@orange.fr>
2020-02-26 09:43:50 +0100

    Compatibility with TYPO3 V10

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '9.5.0',
+    'version' => '10.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99'
+            'typo3' => '9.5.0-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => []
