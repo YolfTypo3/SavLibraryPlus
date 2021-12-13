63bd6cbc2753365d4d4773095a6e3f844d39ecc5
yolf <yolf.typo3@orange.fr>
2021-12-13 10:36:13 +0100

    files deleted

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,7 +0,0 @@
-<?php
-// For compatibility due to the vendor name change.
-return [
-    'SAV\\SavLibraryPlus\\Controller\\Controller' => 'YolfTypo3\\SavLibraryPlus\\Controller\\Controller',
-    'SAV\\SavLibraryPlus\\Filters\\AbstractFilter' => 'YolfTypo3\\SavLibraryPlus\\Filters\\AbstractFilter',
-    'SAV\\SavLibraryPlus\\ViewHelpers' => 'YolfTypo3\\SavLibraryPlus\\ViewHelpers',
-];
